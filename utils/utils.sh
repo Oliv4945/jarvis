354dffbb0e6461b182480275f55d98a5e74aeeac
Oliv4945 <oliv4945@gmail.com>
2017-05-03 20:08:50 +0000

    Lowercase by awk & iconv update

--- utils/utils.sh
+++ utils/utils.sh
@@ -203,7 +203,6 @@ jv_sanitize () {
     # lowercase, remove accents and strip special chars http://stackoverflow.com/a/30832719
     echo $string \
         | awk '{print tolower($0)}' \
-        | iconv -f utf-8 -t ascii//TRANSLIT \
         | sed "s/[^a-zA-Z0-9 $replace_spaces_with]//g"
 }
 
